550495d1ad46918095e7164c3e40577d5ea07267
loveencounterflow <paragate@gmx.net>
2018-02-23 22:45:07 +0100

    updates from m3

---
+++ db/_buildscript-header.sql
@@ -0,0 +1,13 @@
+
+/* turn off pager, to avoid accidental paging */
+\pset pager off
+\pset null '∎'
+-- \pset tuples_only on
+\set _intershop_trm_orange    '\x1b[38;05;208m'
+\set _intershop_trm_yellow    '\x1b[38;05;226m'
+\set _intershop_trm_reset     '\x1b[0m'
+\set _intershop_trm_reverse   '\x1b[7m'
+\set _intershop_trm_title     :_intershop_trm_yellow:_intershop_trm_reverse'  ':_intershop_trm_reset:_intershop_trm_yellow' '
+
+
+
