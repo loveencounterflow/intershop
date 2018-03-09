2403f33a55391ee7a5a64af7d0862ddc96c43839
loveencounterflow <paragate@gmx.net>
2018-03-09 17:37:01 +0100

    refactor buildscript to allow for executable files

---
+++ intershop_modules/buildscript-header.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+# /* turn off pager, to avoid accidental paging */
+# \pset pager off
+# \pset null '∎'
+# -- \pset tuples_only on
+# \set _intershop_trm_orange    '\x1b[38;05;208m'
+# \set _intershop_trm_yellow    '\x1b[38;05;226m'
+# \set _intershop_trm_pink      '\x1b[38;05;199m'
+# \set _intershop_trm_olive     '\x1b[38;05;100m'
+# \set _intershop_trm_plum      '\x1b[38;05;176m'
+# \set _intershop_trm_reset     '\x1b[0m'
+# \set _intershop_trm_reverse   '\x1b[7m'
+# \set _intershop_trm_title     :_intershop_trm_yellow:_intershop_trm_reverse'  ':_intershop_trm_reset:_intershop_trm_yellow' '
+# \set _intershop_trm_title2    :_intershop_trm_plum:_intershop_trm_reverse'  ':_intershop_trm_reset:_intershop_trm_plum' '
+
+
+
