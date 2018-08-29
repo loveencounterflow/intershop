e7787029d63230455adfa2309a051a2182e42b76
loveencounterflow <paragate@gmx.net>
2018-08-29 17:45:10 +0200

    refactoring variable handling to own module (VAR)

--- db/040-var.sql
+++ db/040-var.sql
@@ -1,4 +1,20 @@
 
+
+/*
+
+ .d88888b.  888      8888888b.
+d88P" "Y88b 888      888  "Y88b
+888     888 888      888    888
+888     888 888      888    888
+888     888 888      888    888
+888     888 888      888    888
+Y88b. .d88P 888      888  .d88P
+ "Y88888P"  88888888 8888888P"
+
+*/
+
+
+
 -- =========================================================================================================
 -- VARIABLES
 -- ---------------------------------------------------------------------------------------------------------
@@ -87,6 +103,66 @@ create function U._get_env_variable( ¶key text ) returns text stable language s
 
 -- ---------------------------------------------------------------------------------------------------------
 \ir './update-os-env.sql'
+
+
+/*
+
+888b    888 8888888888 888       888
+8888b   888 888        888   o   888
+88888b  888 888        888  d8b  888
+888Y88b 888 8888888    888 d888b 888
+888 Y88b888 888        888d88888b888
+888  Y88888 888        88888P Y88888
+888   Y8888 888        8888P   Y8888
+888    Y888 8888888888 888P     Y888
+
+*/
+
+
+-- ---------------------------------------------------------------------------------------------------------
+-- drop schema if exists VAR cascade;
+create schema if not exists VAR;
+
+-- ---------------------------------------------------------------------------------------------------------
+create or replace function VAR.get( ¶key text )
+  returns text stable language plpgsql as $$
+  declare
+    ¶row_count  integer;
+    R           text;
+  begin
+    R := value from U.variables where key = ¶key;
+    get diagnostics ¶row_count = row_count;
+    if ¶row_count != 1 then raise exception 'variable not found: %', ¶key; end if;
+    return R;
+    end; $$;
+
+-- ---------------------------------------------------------------------------------------------------------
+create or replace function VAR.get( ¶key text, ¶default anyelement )
+  returns text stable language plpgsql as $$
+  declare
+    ¶row_count  integer;
+    R           text;
+  begin
+    R := value from U.variables where key = ¶key;
+    get diagnostics ¶row_count = row_count;
+    if ¶row_count = 0 then return ¶default; end if;
+    if ¶row_count != 1 then raise exception 'variable not found: %', ¶key; end if;
+    return R;
+    end; $$;
+
+-- ---------------------------------------------------------------------------------------------------------
+create or replace function VAR.set( ¶key text, ¶value anyelement )
+  returns void volatile language sql as $$
+  insert into U.variables values ( ¶key, ¶value )
+  on conflict ( key ) do update set value = ¶value; $$;
+
+-- -- ---------------------------------------------------------------------------------------------------------
+-- create or replace function VAR.refresh( ¶key text, ¶value anyelement )
+--   returns void volatile language sql as $$
+--   select 42; $$;
+
+-- select * from U.variables where key ~ 'mojikura' order by key; xxx;
+
 
 -- \set pwd `pwd`
 -- \echo :pwd
