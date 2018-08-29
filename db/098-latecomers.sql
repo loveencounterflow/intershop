3cd7c4990b9198042f26b2376fc9111be7730b24
loveencounterflow <paragate@gmx.net>
2018-08-29 17:45:37 +0200

    add latecomers script for quick stuff

---
+++ db/098-latecomers.sql
@@ -0,0 +1,23 @@
+
+
+/* This file contains updates to the DB that have not yet been integrated
+into their proper places; it is intended to be runnable both during a regular
+rebuild as well as anytime in between and cause minimal disruption downstream;
+therefore, */
+
+
+/* ###################################################################################################### */
+\ir './010-trm.sql'
+\pset pager off
+\pset tuples_only off
+\timing on
+-- \set ECHO queries
+
+xxx;
+
+/* ====================================================================================================== */
+\echo :red ———{ 89821 quit }———:reset
+\quit
+
+
+
