6819bdadca8f344d45baef44ab6a5304a773738e
loveencounterflow <flow@orion.nebula>
2019-12-06 10:06:51 +0100

    streamline implementation of `IPC.send()`, `IPC.rpc()`

--- db/021-ipc.sql
+++ db/021-ipc.sql
@@ -24,19 +24,6 @@ transaction.
 drop schema if exists IPC cascade;
 create schema IPC;
 
-
-/*
-                                888
-                                888
-                                888
-.d8888b   .d88b.  88888b.   .d88888
-88K      d8P  Y8b 888 "88b d88" 888
-"Y8888b. 88888888 888  888 888  888
-     X88 Y8b.     888  888 Y88b 888
- 88888P'  "Y8888  888  888  "Y88888
-
-*/
-
 -- current_database()
 -- select current_setting('application_name');
 
@@ -54,26 +41,16 @@ reset role;
 create function IPC.send( ¶key text, ¶value jsonb ) returns void volatile language sql as $$
   select IPC.send( ¶key, ¶value, false ); $$;
 
-
 -- =========================================================================================================
 -- RPC
 -- ---------------------------------------------------------------------------------------------------------
 set role dba;
-create function IPC._read_line() returns text volatile language plpython3u as $$
+create function IPC.rpc( key text, value jsonb ) returns jsonb volatile language plpython3u as $$
   plpy.execute( 'select U.py_init()' ); ctx = GD[ 'ctx' ]
-  return ctx.ipc._read_line()
+  import json
+  return ctx.ipc.rpc( key, json.loads( value ) )
   $$;
 reset role;
-
--- ---------------------------------------------------------------------------------------------------------
-create function IPC.rpc( method text, parameters jsonb ) returns jsonb volatile language plpgsql as $$
-    declare
-      R jsonb;
-    begin
-      perform IPC.send( method, parameters, true );
-      R := ( IPC._read_line() )::jsonb;
-      return R->'$value';
-      end; $$;
 
 
 
