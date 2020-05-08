103ca7c961012bb6325cc37b77ac023dd020ebdc
loveencounterflow <flow@orion.nebula>
2020-05-08 14:04:33 +0200

    add SQL submodule for immutable records

---
+++ db/053-immutable.sql
@@ -0,0 +1,38 @@
+
+
+
+/* ###################################################################################################### */
+\ir './010-trm.sql'
+-- \ir './set-signal-color.sql'
+-- \ir './test-begin.sql'
+-- \pset pager on
+\timing off
+\set filename intershop/053-immutable.sql
+\set signal :green
+
+-- ---------------------------------------------------------------------------------------------------------
+\echo :signal ———{ :filename 1 }———:reset
+drop schema if exists IMMUTABLE cascade; create schema IMMUTABLE;
+
+-- ---------------------------------------------------------------------------------------------------------
+\echo :signal ———{ :filename 2 }———:reset
+create function IMMUTABLE.record_has_changed( old record, new record, excludes text[] )
+  returns boolean language plpgsql as $$ begin
+    return ( array_length( akeys( hstore( new ) - hstore( old ) - excludes ), 1 ) > 0 ); end; $$;
+
+create function IMMUTABLE.record_has_changed( old record, new record ) returns boolean language plpgsql as $$
+  begin
+    return IMMUTABLE.record_has_changed( old, new, array[] ); end; $$;
+
+
+
+/* ###################################################################################################### */
+\echo :red ———{ :filename 3 }———:reset
+\quit
+
+
+
+
+
+
+
