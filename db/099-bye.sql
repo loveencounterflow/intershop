cdb6e01328c7eb82899c8d5f958caaa1cdde640d
flow <flow@enceladus.orbit>
2020-07-09 16:44:01 +0200

    remove output from submodule `099-bye`

--- db/099-bye.sql
+++ db/099-bye.sql
@@ -24,12 +24,12 @@
 -- do $$ begin perform log( 'U.variables' ); end; $$;
 -- select * from U.variables order by key \g :out
 
-do $$ begin perform log(); end; $$;
-do $$ begin perform log( 'U.variables (excerpt):' ); end; $$;
-select * from U.variables
-  where not key ~ '^os/'
-  order by key \g :out
+-- do $$ begin perform log(); end; $$;
+-- do $$ begin perform log( 'U.variables (excerpt):' ); end; $$;
+-- select * from U.variables
+--   where not key ~ '^os/'
+--   order by key \g :out
 
-do $$ begin perform log( 'PostGreSQL version:', version() ); end; $$;
+-- do $$ begin perform log( 'PostGreSQL version:', version() ); end; $$;
 
 
