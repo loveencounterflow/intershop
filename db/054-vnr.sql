20feee8ee2da344b8c2d5996d79b139c210501a5
flow <flow@enceladus.orbit>
2020-07-23 18:53:02 +0200

    `vnr`: implement `push()`, `cat()`, discard `push_zero()` (use `push()` with single argument instead)

--- db/054-vnr.sql
+++ db/054-vnr.sql
@@ -27,9 +27,16 @@ create domain VNR.vnr as float8[] check ( coalesce( array_length( value, 1 ) ) >
 
 -- ---------------------------------------------------------------------------------------------------------
 \echo :signal ———{ :filename 2 }———:reset
-create function VNR.push_zero( ¶vnr VNR.vnr )
+create function VNR.push( ¶vnr VNR.vnr, ¶nr float8 default 0 )
   returns VNR.vnr immutable parallel safe language sql as $$
-  select array_append( ¶vnr, 0::float8 )::VNR.vnr; $$;
+  select array_append( ¶vnr, ¶nr )::VNR.vnr; $$;
+
+-- ---------------------------------------------------------------------------------------------------------
+\echo :signal ———{ :filename 2 }———:reset
+create function VNR.cat( ¶vnr1 VNR.vnr, ¶vnr2 VNR.vnr )
+  returns VNR.vnr immutable parallel safe language sql as $$
+  select array_cat( ¶vnr1, ¶vnr2 )::VNR.vnr; $$;
+
 
 
 /* ###################################################################################################### */
